48d692aff188fbd5aecee9a62e8272844c129998
Rizwan <64288750+frizwankhan@users.noreply.github.com>
2021-03-12 21:50:40 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import tensorflow as tf
+from tensorflow import keras
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+import string
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('/content/data.csv')
+
+def remove_punctuations(text):
+    for punctuation in string.punctuation:
+        text = text.replace(punctuation, '')
+    return text
+
+df['review'] = df['review'].apply(remove_punctuations)
+
+tokenizer=RegexpTokenizer(r'\w+')
+df['review']=df['review'].apply(lambda x: tokenizer.tokenize(x.lower()))
+
+# def removestopwords(s):
+#   words=[i for i in s if i not in stopwords.words('english')]
+#   print(words)
+#   return words
+
+x = df.iloc[:,0].values
+y = df.iloc[:,1].values
+
+def dummy_fun(doc):
+    return doc
+
+vectorizer = TfidfVectorizer(
+    analyzer='word',
+    tokenizer=dummy_fun,
+    preprocessor=dummy_fun,
+    token_pattern=None)  
+
+x = vectorizer.fit_transform(x)
+x = x.toarray()
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.05,random_state = 0,stratify = y)
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+# y = keras.utils.to_categorical(y)
+# y = y[:, 1:]
+
+y_train = y_train[:, 1:]
+y_test = y_test[:, 1:]
+
+model = keras.models.Sequential([keras.layers.BatchNormalization(input_dim=x_train.shape[1]),
+                                 keras.layers.Dense(100,kernel_initializer="he_normal"),
+                                 keras.layers.PReLU(),
+                                 keras.layers.BatchNormalization(),
+                                 keras.layers.Dropout(0.2),
+                                 keras.layers.Dense(50,kernel_initializer="he_normal"),
+                                 keras.layers.PReLU(),
+                                 keras.layers.BatchNormalization(),
+                                 keras.layers.Dense(5, "softmax"),
+                                 ])
+
+model.compile(loss="categorical_crossentropy",
+              optimizer=keras.optimizers.Adam(),
+              metrics="accuracy")
+
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)
+
+history = model.fit(x_train, y_train,validation_data=(x_test,y_test),
+                    epochs=50, callbacks=[early_stopping_cb])
+
+st.title('sentiment analysis')
+select = st.text_input('Enter your message')
+
+select = remove_punctuations(select)
+select = tokenizer.tokenize(select.lower())
+select = vectorizer.transform([select]).toarray()
+output = np.argmax(model.predict(select), axis=-1)
+
+st.title(output[0])
